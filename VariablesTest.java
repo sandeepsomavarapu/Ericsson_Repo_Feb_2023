192c9bf3b539b35d3ab0a4669f238f44a0b79e52
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-02-20 13:15:15 +0530

    Day-1 Morning Session

---
+++ VariablesTest.java
@@ -0,0 +1,16 @@
+
+public class VariablesTest {
+	int accNo=123123123;//global variables
+	public static void main(String[] args) {
+		byte age = 28;//local variables
+		long contact = 9494949494l;
+		float salary = 19.546372342f;
+		double value = 90.2342342342342;
+		char gender = 'm';
+		boolean result = false;
+		boolean condition=true;
+
+		System.out.println(age + " " + contact + " " + salary + " " + value + " " + gender + " " + result);
+	}
+
+}
---
+++ Welcome.java
@@ -0,0 +1,12 @@
+
+class Welcome {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("welcome to java eclipse");
+	}
+}
+
+//ctrl+shift+f -->formatting
+//ctrl+space-->suggestions
+//ctrl+\ -->to comment 
