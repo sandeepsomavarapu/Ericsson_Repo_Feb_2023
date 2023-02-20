4a062dd73f8ca6a9b7283d3e32ee599d5e99f30c
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-02-20 16:24:17 +0530

    Day-1 Afternoon Session

--- VariablesTest.java
+++ VariablesTest.java
@@ -1,16 +1,35 @@
 
 public class VariablesTest {
-	int accNo=123123123;//global variables
+	public int addOfTwo(int a, int b) {
+		return a + b;
+	}
+
+	public static void subOfTwo(int a, int b) {
+		int result = a - b;
+		System.out.println("sub of two numbers : " + result);
+	}
+
+	public void mulofTwo() {
+		int result = 200 * 300;
+		System.out.println("mul of two numbers : " + result);
+	}
+
+	static int countryCode = 91;// static variable
+	int empId = 1231; // instnace variable -->instance and static 4 bytes
+
 	public static void main(String[] args) {
-		byte age = 28;//local variables
+		byte age = 28;// local variables
 		long contact = 9494949494l;
 		float salary = 19.546372342f;
 		double value = 90.2342342342342;
 		char gender = 'm';
 		boolean result = false;
-		boolean condition=true;
-
 		System.out.println(age + " " + contact + " " + salary + " " + value + " " + gender + " " + result);
+		System.out.println(VariablesTest.countryCode);
+		VariablesTest.subOfTwo(500, 250);
+		VariablesTest obj = new VariablesTest();
+		System.out.println(obj.empId);
+		System.out.println(obj.addOfTwo(13, 14));
+		obj.mulofTwo();
 	}
-
 }
--- Welcome.java
+++ Welcome.java
@@ -1,10 +1,38 @@
 
 class Welcome {
-	
-	
+
 	public static void main(String[] args) {
-		System.out.println("welcome to java eclipse");
+		if (20==20) {
+			int x=123;}
+		else
+			System.out.println("am from else ");
+		
+		
+		switch ("sandeep") {
+		case "sandeep":
+			System.out.println("hello sandeep");
+			break;
+		case "hello":
+			System.out.println("hello");
+			break;
+		case "morning":
+			System.out.println("good morning");
+			break;
+			
+		default:
+			System.out.println("not matched.... ");
+			break;
+		}	
+		
+		
+		for (int i = 1; i <= 100; i++) {
+			if(i%2!=0)
+				System.out.println(i);	
+		}
 	}
+	
+	
+	
 }
 
 //ctrl+shift+f -->formatting
