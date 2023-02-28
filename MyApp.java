ccee4dcae626eca22b12989023f00cf57af96b42
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-02-28 15:16:05 +0530

    Threads synchronized keyword

--- MyApp.java
+++ MyApp.java
@@ -4,14 +4,27 @@ public class MyApp implements Runnable {
 	@Override
 	public void run() {
 		for (int i = 0; i < 5; i++) {
-			System.out.println("Thread created ...");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+			System.out.println("child thread ...");
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		MyApp app = new MyApp();
-		Thread th = new Thread(app);
-		th.start();//run() 
-	}
+		Thread th = new Thread(app);// start
+		th.start();// run() runnable-->ready run
+		th.join();
+		for (int i = 0; i < 5; i++) {
 
-}
+			System.out.println("main thread ...");
+		}
+	}
+	// waiting
+} // | sleep,yield,wait...
+//Initial--->runnable-->running --->dead/end
+// run()
--- MyApp1.java
+++ MyApp1.java
@@ -7,8 +7,9 @@ public class MyApp1 extends Thread {
 		System.out.println(Thread.currentThread().getPriority());//5
 		Thread.currentThread().setName("sandeep's");
 		Thread.currentThread().setPriority(10);
-		System.out.println(Thread.currentThread().getName());//main
-		System.out.println(Thread.currentThread().getPriority());//5
+		System.out.println(Thread.currentThread());
+//		System.out.println(Thread.currentThread().getName());//main
+//		System.out.println(Thread.currentThread().getPriority());//5
 		for (int i = 0; i < 10; i++) {//1-10
 			System.out.println("child thread");//5,thread-0 extended from parent
 		}
@@ -24,6 +25,12 @@ public class MyApp1 extends Thread {
 		MyApp1 app = new MyApp1();
 		app.start();
 		for (int i = 0; i < 10; i++) {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.println("main thread");//5,main
 		}
 	}
