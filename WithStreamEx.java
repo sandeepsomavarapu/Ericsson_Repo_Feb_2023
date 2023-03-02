222bc10e269bbfb1f5aa160e6ff44bc6e606076c
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-03-02 15:23:19 +0530

    Stream API Ex Day-9

---
+++ WithStreamEx.java
@@ -0,0 +1,21 @@
+package com.ericcson.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WithStreamEx {
+	public static void main(String[] args) {
+		List<String> names = new ArrayList<String>();
+		names.add("Ajeet");
+		names.add("Negan");
+		names.add("Aditya");
+		names.add("Steve");
+
+		// Using Stream and Lambda expression
+		long count = names.parallelStream().filter(str -> str.length() < 6).count();
+		System.out.println("There are " + count + " strings with length less than 6");
+		
+		
+
+	}
+}
